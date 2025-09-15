6b3072ea1b56bf59d21cc44f5fd9d2c76ff92347
IT24103027 <IT24103027@my.sliit.lk>
2025-09-15 23:46:52 +0530

    IT24103027 LAB 06

---
+++ Lab Sheet 06/IT24103027 PS LAB 06.R
@@ -0,0 +1,17 @@
+setwd("C:\\Users\\LOHITHA HETTIGE\\Desktop\\IT24103027")
+#Q1
+#i.
+#binomial distribution
+
+#ii.
+pbinom(46,50,0.85,lower.tail =FALSE)
+
+#Q2
+#i.
+#number of customer calls per hour
+
+#ii.
+#poisson distribution
+
+#iii.|
+dpois(15,12)
